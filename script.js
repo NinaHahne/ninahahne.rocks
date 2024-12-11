ee6572f28f89a143e7dd111bf6b4d22d96dfca3d
NinaHahne <ninamariehahne@gmail.com>
2024-12-11 16:29:26 +0100

    Wheel "HIRE ME NOW" rotates onscroll

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 // Rotating Wheel:
 const wheel = document.querySelector('.wheel-wrapper');
 const SPEED = 1;
-let progress = 0.5; // 0-1 change while scrolling
-let wheelRotation = -360 * Math.min(1, progress * SPEED); // deg
+const initialRotation = -29;
+let progress = 0; // 0-1 change while scrolling
 
-// TODO:
-// wheel.style.setProperty('--wheelRotation', wheelRotation+'deg');
+// progress should be 0 when scrolled to the top and 1 when scrolled to the bottom:
+window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    progress = scrollTop / (scrollHeight - clientHeight);
+    
+    let wheelRotation = -360 * Math.min(1, progress * SPEED) + initialRotation; // deg
+    wheel.style.setProperty('--wheelRotation', wheelRotation+'deg');
+});
